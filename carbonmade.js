398590a6a5c5b7cef87870cd423189f57a0ec97e
Kyle Fox <kyle.fox@gmail.com>
2011-01-13 15:37:18 -0700

    Added an action for the Order list

--- carbonmade.js
+++ carbonmade.js
@@ -10,7 +10,17 @@ function index_url(model) {
   return [SITE, model].join("/");
 };
 
-carbonmade.get = function(model, id, callback) {
+carbonmade.get = function() {
+  var args = Array.prototype.slice.call(arguments),
+      model = args[0],
+      id, callback;
+  if(args.length === 2) {
+    callback = args[1];
+  } else {
+    id = args[1];
+    callback = args[2];
+  }
   var url = id ? show_url(model, id) : index_url(model);
+
   rest.get(url).addListener('complete', callback);
 };
--- routes/orders.js
+++ routes/orders.js
@@ -1,7 +1,19 @@
 var cm = require('../carbonmade');
 
+// List orders
+app.get('/orders', function(req, res){
+  cm.get('orders', function(orders) {
+    console.log(orders);
+    res.render('orders/index', {
+      title: 'Orders',
+      orders: orders
+    });
+  });
+});
+
+// Order details
 app.get('/orders/:id', function(req, res){
-  cm.get('orders', 1, function(order) {
+  cm.get('orders', req.params.id, function(order) {
     res.render('orders/show', {
       title: 'Order Details',
       order: order
