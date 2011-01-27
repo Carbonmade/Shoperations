a51b12b2f56280cc47600d5316cba614efaf3411
Kyle Fox <kyle.fox@gmail.com>
2011-01-26 18:55:09 -0700

    Refactored Orders routes to use a new Finder class instead of the carbonmade module.

---
+++ finder.js
@@ -0,0 +1,41 @@
+var rest = require('restler'),
+    Finder = module.exports = function(model) { this.model = model; };
+
+Finder.prototype.buildURL = function(id, params) {
+  var url = "http://store.carbonmade.net/" + this.model;
+  if(id) url += "/" + id;
+  if(Object.keys(params).length > 0) {
+    var qs = [];
+    for(key in params) {
+      qs.push(key + "=" + params[key]);
+    }
+    url += "?" + qs.join("&");
+  }
+  return url;
+};
+
+Finder.prototype.find = function() {
+  var url, id = null, params = {}, callback, query;
+  if(arguments.length === 1) {
+    // Single argument is the callback.
+    callback = arguments[0];
+  } else {
+    // Callback is the last argument.
+    callback = arguments[1];
+    if(typeof arguments[0] === 'object') {
+      params = arguments[0]; 
+    }
+    else {
+      id = arguments[0];
+    }
+  }
+  url = this.buildURL(id, params);
+  console.log("GET: " + url);
+  query = rest.get(url);
+  query.addListener('complete', function(data, response){
+    if(response.headers['x-total']) {
+      data.totalRecords = response.headers['x-total'];
+    }
+    callback(data, response);
+  });
+};
--- routes/orders.js
+++ routes/orders.js
@@ -1,21 +1,37 @@
-var cm = require('../carbonmade');
+var Finder = require('../finder'),
+    Orders = new Finder('orders');
 
-// List orders
+function renderOrderList(response, title, orders) {
+  response.render('orders/index', {
+    title: title,
+    orders: orders
+  });
+};
+
+// All orders
 app.get('/orders', function(req, res){
-  cm.get('orders', function(orders) {
-    res.render('orders/index', {
-      title: 'Orders',
-      orders: orders
-    });
+  Orders.find(function(orders) {
+    renderOrderList(res, 'All Orders', orders);
+  });
+});
+
+// Open orders
+app.get('/orders/open', function(req, res){
+  Orders.find({status: "Open"}, function(orders) {
+    renderOrderList(res, 'Open Orders', orders);
+  });
+});
+
+// Processing orders
+app.get('/orders/processing', function(req, res){
+  Orders.find({status: "Processing"}, function(orders) {
+    renderOrderList(res, 'Processing Orders', orders);
   });
 });
 
 // Order details
 app.get('/orders/:id', function(req, res){
-  cm.get('orders', req.params.id, function(order) {
-    res.render('orders/show', {
-      title: 'Order Details',
-      order: order
-    });
+  Orders.find(req.params.id, function(order) {
+    res.render('orders/show', {title: 'Order Details', order: order});
   });
 });
