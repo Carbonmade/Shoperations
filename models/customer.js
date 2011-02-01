8a23501ad9c386c1f0d1499e3475f12c524154e1
Kyle Fox <kyle.fox@gmail.com>
2011-02-01 12:06:28 -0700

    Handle attributes.orders being blank

--- models/customer.js
+++ models/customer.js
@@ -6,11 +6,13 @@ var Customer = exports = module.exports = function Customer(attributes) {
   Model.call(this, attributes);
   
   // Cast `orders` objects into an Array of actual Order instances.
-  var orders = [];
-  attributes.orders.forEach(function(orderAttrs) {
-    orders.push( new Order(orderAttrs) );
-  });
-  this.orders = orders;
+  if(!!attributes.orders) {
+    var orders = [];
+    attributes.orders.forEach(function(orderAttrs) {
+      orders.push( new Order(orderAttrs) );
+    });
+    this.orders = orders; 
+  }
 };
 
 Customer.prototype = new Model;
