4b77c739d86cee07da33d973691376a1c25446c6
Kyle Fox <kyle.fox@gmail.com>
2011-02-01 11:31:17 -0700

    Typo

--- models/order.js
+++ models/order.js
@@ -7,6 +7,6 @@ var Order = exports = module.exports = function Order(attributes) {
 
 Order.prototype = new Model;
 
-Order.prototype.modelName = 'Orders';
+Order.prototype.modelName = 'Order';
 
 Order.objects = new Collection(Order);
