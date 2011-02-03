a2ae7ab8d3451c2d16c08b49039a8ffe79c8f354
Kyle Fox <kyle.fox@gmail.com>
2011-02-03 10:52:12 -0700

    Need to sudo, of course.

--- shoperations.sh
+++ shoperations.sh
@@ -6,9 +6,12 @@
 #   To install:
 #       sudo ln -s /home/ubuntu/Shoperations/shoperations.sh /etc/init.d/shoperations
 #       sudo chmod +x /etc/init.d/shoperations
-#       update-rc.d shoperations defaults
+#       sudo update-rc.d shoperations defaults
+#
+#   When installing, you might see a message: update-rc.d: warning: /etc/init.d/shoperations missing LSB information
+#   You can safely ignore that. It's just complaining that this script doesn't offer the normal {start|stop|restart} commands.
 #
 #   To remove from boot sequence:
-#       update-rc.d -f shoperations remove
+#       sudo update-rc.d -f shoperations remove
 #
 forever start -o /home/ubuntu/Shoperations/log/console.log -e /home/ubuntu/Shoperations/log/error.log /home/ubuntu/Shoperations/app.js
