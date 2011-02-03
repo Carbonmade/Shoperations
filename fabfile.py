57bab224c1f9507e44a8fe6d556911dc2a5e536c
Kyle Fox <kyle.fox@gmail.com>
2011-02-03 12:03:39 -0700

    More fab tasks

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,5 @@
 import os
-from fabric.api import run, sudo, put, env
+from fabric.api import run, sudo, put, env, local
 
 def production():
     env.hosts = ['ec2-204-236-210-184.compute-1.amazonaws.com']
@@ -7,7 +7,12 @@ def production():
     env.key_filename = [os.path.join(os.getenv("HOME"), '.ssh', 'node.pem')]
 
 def deploy():
+    local('git push origin master')
     run('cd /home/ubuntu/Shoperations && git pull origin master')
+
+def restart():
     run('/home/ubuntu/node/local/bin/forever stopall')
     run('/etc/init.d/shoperations')
     run('forever list')
+    sudo('/etc/init.d/nginx stop')
+    sudo('/etc/init.d/nginx start')
