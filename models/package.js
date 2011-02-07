59e649f7420985f43ba485fa47262bf205436eae
Kyle Fox <kyle.fox@gmail.com>
2011-02-07 15:58:42 -0700

    Added Package.STATES enum

--- models/package.js
+++ models/package.js
@@ -14,3 +14,17 @@ Package.objects = new Collection(Package);
 Package.prototype.shipped = function() {
   return this.state === 'Shipped';
 };
+
+Package.STATES = {
+  PROCESSING: 'Processing',
+  SHIPPED: 'Shipped',
+  DELIVERED: 'Delivered'
+};
+
+Package.stateFilters = function() {
+  var filters = {};
+  for(state in Package.STATES)
+    if(Package.STATES.hasOwnProperty(state))
+      filters[Package.STATES[state]] = '/packages/' + state.toLowerCase();
+  return filters;
+};
